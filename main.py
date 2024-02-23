e3a4298f51d57e4c071720cd6600e2253b813b93
yakov-grab <yakov.grab@yandex.ru>
2024-02-23 13:51:07 +0300

    flake8

--- main.py
+++ main.py
@@ -3,27 +3,32 @@ from PIL import Image
 import streamlit as st
 import requests
 
+
 # Load model and processor
 processor = ViTImageProcessor.from_pretrained('google/vit-base-patch16-224')
-model = ViTForImageClassification.from_pretrained('google/vit-base-patch16-224')
+m = ViTForImageClassification.from_pretrained('google/vit-base-patch16-224')
+
 
 # Function to load image from computer
 def load_image_from_file(file):
     image = Image.open(file)
     return image
 
+
 # Function to load image from URL
 def load_image_from_url(url):
     image = Image.open(requests.get(url, stream=True).raw)
     return image
 
+
 # Function to classify image
 def classify_image(image):
     inputs = processor(images=image, return_tensors="pt")
-    outputs = model(**inputs)
+    outputs = m(**inputs)
     logits = outputs.logits
     predicted_class_idx = logits.argmax(-1).item()
-    return model.config.id2label[predicted_class_idx]
+    return m.config.id2label[predicted_class_idx]
+
 
 def main():
     st.set_page_config(layout="wide")
@@ -35,7 +40,8 @@ def main():
 
     # Left column: Buttons, upload fields, and Predict Class button
     with col1:
-        uploaded_file = st.file_uploader("Choose an image:", type=["jpg", "jpeg", "png"])
+        uploaded_file = st.file_uploader("Choose an image:",
+                                         type=["jpg", "jpeg", "png"])
         url = st.text_input("...or enter image URL:")
         if uploaded_file:
             image = load_image_from_file(uploaded_file)
@@ -48,7 +54,7 @@ def main():
         else:
             st.warning("Please upload an image or provide an image URL.")
             return
-        
+
         if st.button("Classify image") and image:
             predicted_class = classify_image(image)
             st.success(f"Image class: {predicted_class}")
@@ -56,17 +62,19 @@ def main():
     # Right column: Display the uploaded image
     with col2:
         if image:
-            # To ensure that square images do not use the entire width of the column
             aspect_ratio = image.width / image.height
             use_column_width = aspect_ratio > 1.4
 
             if uploaded_file:
-                st.image(image, caption="Uploaded image", use_column_width=use_column_width)
+                st.image(image, caption="Uploaded image",
+                         use_column_width=use_column_width)
             elif url:
                 try:
-                    st.image(image, caption="Image from URL", use_column_width=use_column_width)
+                    st.image(image, caption="Image from URL",
+                             use_column_width=use_column_width)
                 except Exception as e:
                     st.warning(f"Error loading image from URL: {e}")
+
 
 if __name__ == "__main__":
     main()
--- test_main.py
+++ test_main.py
@@ -1,16 +1,18 @@
-import pytest
 from PIL import Image
 from main import load_image_from_file, load_image_from_url, classify_image
+
 
 def test_load_image_from_file():
     temp_file = "img/red.png"
     loaded_image = load_image_from_file(temp_file)
     assert isinstance(loaded_image, Image.Image)
 
+
 def test_load_image_from_url():
     url = "https://avatars.githubusercontent.com/u/92524901?v=4"
     loaded_image = load_image_from_url(url)
     assert isinstance(loaded_image, Image.Image)
+
 
 def test_classify_image():
     file_path = "img/red.png"
